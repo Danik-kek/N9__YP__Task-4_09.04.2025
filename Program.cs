3988669703eff0a44ddf05a6de1c843cbddb739f
Aunt D <dzavizistur@gmail.com>
2025-04-09 12:40:15 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace N9__YP__Task_4_09._04._2025
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string FilePath = "Karl.txt";
+
+            if (!File.Exists(FilePath)) 
+            {
+                Console.WriteLine("file not found!");
+                return;
+            }
+
+            string text = File.ReadAllText(FilePath);
+            int count = 0;
+            Console.WriteLine(text);
+
+
+            foreach (char c in text)
+            {
+                if (c == 'а') count ++;
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
